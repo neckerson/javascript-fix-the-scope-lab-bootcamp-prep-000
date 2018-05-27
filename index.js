5a1b915287c113539412941b035d85881a0b76c7
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-27 03:55:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 var funk = funkyFunction()
-a = funk()
+var a = funk()
 
 console.log(a)
 theFunk = a
